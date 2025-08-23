b2f21ac94d1f83cc4662b86f1da9c4285641ff9d
DELL <DELL@DESKTOP-TIUFNN9>
2025-08-24 00:18:58 +0600

    Add project files.

---
+++ CHERRY/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace CHERRY
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ CHERRY/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CHERRY")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CHERRY.Pages")]
