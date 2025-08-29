d07a07c2cf2290ca8c50a25af2d566967482857e
Medha <Medha@MEDHA>
2025-08-29 16:21:13 +0600

    primary login-reg

--- CHERRY/App.xaml.cs
+++ CHERRY/App.xaml.cs
@@ -1,15 +1,23 @@
-﻿namespace CHERRY
+﻿using CHERRY.Services;
+using CHERRY.Views;
+using Microsoft.Maui.Controls;
+
+namespace CHERRY
 {
     public partial class App : Application
     {
-        public App()
+        private readonly DatabaseService _db;
+
+        public App(DatabaseService db)
         {
             InitializeComponent();
+            _db = db;
+            // Removed MainPage assignment to avoid CS0618
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new AppShell());
+            return new Window(new NavigationPage(new LoginPage(_db)));
         }
     }
 }
