82f0f8a790de7085215fdef498af6079e293f121
Arkanullah Saad <120630818+arkanullahs@users.noreply.github.com>
2025-09-06 16:00:45 +0600

    cycleapiupdate

--- CHERRY/Services/CycleApiService.cs
+++ CHERRY/Services/CycleApiService.cs
@@ -34,13 +34,39 @@ namespace CHERRY.Services
 			return res.IsSuccessStatusCode;
 		}
 
+		public async Task<bool> UpdateAsync(int id, DateTime startDate, DateTime endDate)
+		{
+			if (!await _authService.EnsureAuthHeaderAsync()) return false;
+			var payload = new UpdateCycleDto
+			{
+				StartDate = DateOnly.FromDateTime(startDate.Date),
+				EndDate = DateOnly.FromDateTime(endDate.Date)
+			};
+			var res = await _httpClient.PutAsJsonAsync($"api/cycles/{id}", payload);
+			return res.IsSuccessStatusCode;
+		}
+
+		public async Task<bool> DeleteAsync(int id)
+		{
+			if (!await _authService.EnsureAuthHeaderAsync()) return false;
+			var res = await _httpClient.DeleteAsync($"api/cycles/{id}");
+			return res.IsSuccessStatusCode;
+		}
+
 		public class CycleDto
 		{
+			public int Id { get; set; }
 			public DateOnly StartDate { get; set; }
 			public DateOnly EndDate { get; set; }
 		}
 
 		private class CreateCycleDto
+		{
+			public DateOnly StartDate { get; set; }
+			public DateOnly EndDate { get; set; }
+		}
+
+		private class UpdateCycleDto
 		{
 			public DateOnly StartDate { get; set; }
 			public DateOnly EndDate { get; set; }
